0034f8bdb61493e2e66a540be2f6de98a78c6db8
Ivan-1106 <alexivanedquendiaz@gmail.com>
2025-11-09 10:21:02 -0500

    Update TrasporteUrbanoMariaInes.java
    Mi primer saludo en en este git

--- src/trasporteurbano/mariaines/TrasporteUrbanoMariaInes.java
+++ src/trasporteurbano/mariaines/TrasporteUrbanoMariaInes.java
@@ -7,6 +7,7 @@ public class TrasporteUrbanoMariaInes {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mis amiguito");
+        System.out.println("Hola Anderson y Rodrigo");
     }
     
 }
