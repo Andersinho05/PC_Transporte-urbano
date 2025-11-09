4c83227968e85c1d5353c15702911456d1c24d71
Ander <Andercabreram@gmail.com>
2025-11-09 10:03:48 -0500

    Mi primer aporte(Alexis)

---
+++ src/trasporteurbano/mariaines/TrasporteUrbanoMariaInes.java
@@ -0,0 +1,12 @@
+
+package trasporteurbano.mariaines;
+
+
+public class TrasporteUrbanoMariaInes {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mis amiguito");
+    }
+    
+}
