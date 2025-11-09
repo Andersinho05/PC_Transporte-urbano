3787ac90f7e63907663bd9ea61f3c248f722d36f
Ander <Andercabreram@gmail.com>
2025-11-09 10:35:23 -0500

    Creando la clase bus(Anderson)

---
+++ src/trasporteurbano/mariaines/Bus.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trasporteurbano.mariaines;
+
+/**
+ *
+ * @author ANDERSON
+ */
+public class Bus {
+ private String  placa;
+
+    public Bus() {
+    }
+    
+  
+   
+
+}
